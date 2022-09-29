47855c7dd25adb6b03b60bedd2fbb8d52bc19233
prajwal971 <prajwalkumar971@gmail.com>
2022-09-30 03:12:51 +0400

    Single Post Page Design user click post write edit design completed

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
+import {Link} from 'react-router-dom'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className="content">
+        <img src="" alt="" />
+        <div className="user">
+          <img src="" alt="" />
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+
+          <div className="edit">
+            <Link to='/write?edit=2'>
+              <img src={Edit} alt="" />
+            </Link>
+
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="menu">m</div>
+    </div>
   )
 }
 
