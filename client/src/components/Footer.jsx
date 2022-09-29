6ecff0b3198a57f375a42aafec61af67c7d32120
prajwal971 <prajwalkumar971@gmail.com>
2022-09-30 00:47:49 +0400

    Footer design complete & Home page functionality added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Logo from "../img/logo.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made with ♥️ and <b>React.Js</b>.
+      </span>
+    </footer>
   )
 }
 
