278acd8a8c09a107e7a3d3788913ed63aaae90d7
prajwal971 <prajwalkumar971@gmail.com>
2022-10-01 11:36:27 +0400

    Add New Post Page Design completed

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,30 +1,49 @@
 import React from 'react'
 import Edit from '../img/edit.png'
 import Delete from '../img/delete.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Menu from '../components/Menu'
 
 const Single = () => {
   return (
-    <div className='single'>
+    <div className="single">
       <div className="content">
-        <img src="" alt="" />
+        <img src="https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
         <div className="user">
-          <img src="" alt="" />
+          <img src="https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
           <div className="info">
             <span>John</span>
             <p>Posted 2 days ago</p>
           </div>
-
           <div className="edit">
             <Link to='/write?edit=2'>
               <img src={Edit} alt="" />
             </Link>
-
             <img src={Delete} alt="" />
           </div>
         </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
+
+        </p>
       </div>
-      <div className="menu">m</div>
+      <Menu />
     </div>
   )
 }
