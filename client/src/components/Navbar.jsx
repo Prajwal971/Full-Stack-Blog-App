6a3fe98f4ede9c216f33d591cd590f20e24c6d6c
prajwal971 <prajwalkumar971@gmail.com>
2022-10-02 15:57:42 +0400

    Context API added,Login and Logout functionality added.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Logo from "../img/logo.png"
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
 
 const Navbar = () => {
+
+  const { currentUser, logout } = useContext(AuthContext);
+
+
   return (
     <div className='navbar'>
       <div className="container">
@@ -28,8 +34,14 @@ const Navbar = () => {
           <Link className='link' to="/?cat=art">
             <h6>FOOD</h6>
           </Link>
-          <span>John</span>
-          <span>Logout</span>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className='link' to="/Login">
+              Login
+            </Link>
+          )}
           <span className='write'>
             <Link className='link' to="/write">Write</Link>
           </span>
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthContectProvider } from './context/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContectProvider>
+      <App />
+    </AuthContectProvider>
   </React.StrictMode>
 );
 
