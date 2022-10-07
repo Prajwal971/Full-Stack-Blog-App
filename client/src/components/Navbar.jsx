4ad4202432cc2dc07c37a2eeb31eaaf25a8ed3e1
prajwal971 <prajwalkumar971@gmail.com>
2022-10-08 02:45:06 +0400

    Posts fetch added and edit and update functionality added.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,25 +13,27 @@ const Navbar = () => {
     <div className='navbar'>
       <div className="container">
         <div className="logo">
-          <img src={Logo} alt="" />
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
         </div>
         <div className="links">
           <Link className='link' to="/?cat=art">
             <h6>ART</h6>
           </Link>
-          <Link className='link' to="/?cat=art">
+          <Link className='link' to="/?cat=science">
             <h6>SCIENCE</h6>
           </Link>
-          <Link className='link' to="/?cat=art">
+          <Link className='link' to="/?cat=tech">
             <h6>TECHNOLOGY</h6>
           </Link>
-          <Link className='link' to="/?cat=art">
+          <Link className='link' to="/?cat=cinema">
             <h6>CINEMA</h6>
           </Link>
-          <Link className='link' to="/?cat=art">
+          <Link className='link' to="/?cat=design">
             <h6>DESIGN</h6>
           </Link>
-          <Link className='link' to="/?cat=art">
+          <Link className='link' to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
           <span>{currentUser?.username}</span>
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,44 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Edit from '../img/edit.png'
 import Delete from '../img/delete.png'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Menu from '../components/Menu'
+import axios from 'axios'
+import moment from 'moment'
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
 
 const Single = () => {
+  const [post, setPost] = useState({});
+
+  const location = useLocation()
+
+  const postId = location.pathname.split("/")[2]
+
+  const {currentUser} = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`)
+        setPost(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchdata()
+  }, [postId])
+
+
   return (
     <div className="single">
       <div className="content">
-        <img src="https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <img src={post?.img} alt="" />
         <div className="user">
           <img src="https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
           <div className="info">
-            <span>John</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow() } </p>
           </div>
-          <div className="edit">
+          {currentUser.username === post.username && <div className="edit">
             <Link to='/write?edit=2'>
               <img src={Edit} alt="" />
             </Link>
             <img src={Delete} alt="" />
-          </div>
+          </div>}
         </div>
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!
 
         </p>
